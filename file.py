613be23f15a3763249cae5d7e8c25aee7a3058a3
derick naligi <chahasid@gmail.com>
2024-09-25 15:25:09 +0300

    commiting my las line

--- file.py
+++ file.py
@@ -2,4 +2,4 @@ str_er = int((input(" weight in  kg ")))
 if str_er > 10:
     print("Minor")
 else:
-    print("Adult below one year KINDLY REGESTER FOR ID CARD ") 
+    print("Adult below one year KINDLY REGESTER FOR ID CARD GIT ") 
